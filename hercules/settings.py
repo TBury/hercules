b280be6af3d2bbfca0daa5088702f5f076a534b8
TBury <tomasz.bury@onet.eu>
2020-06-06 12:53:40 +0200

    Debugging problem

--- hercules/settings.py
+++ hercules/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'm^%^y3_8e&$l)q&mfuihrm_gihvl#g7@k&gpfg4bwx8v9kpu#8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['hercules-project.herokuapp.com']
 
