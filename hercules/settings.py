62e2c811967c60c93f998581bc82af9b9a845e54
TBury <tomasz.bury@onet.eu>
2020-06-06 12:33:21 +0200

    add heroku allowed host

--- hercules/settings.py
+++ hercules/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'm^%^y3_8e&$l)q&mfuihrm_gihvl#g7@k&gpfg4bwx8v9kpu#8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['hercules-project.herokuapp.com']
 
 
 # Application definition
