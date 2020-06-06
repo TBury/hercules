2d7cd938cfd2682705057ee22094afb7656affcc
TBury <tomasz.bury@onet.eu>
2020-06-06 12:57:17 +0200

    debugging problem v2

--- hercules/settings.py
+++ hercules/settings.py
@@ -161,3 +161,19 @@ CELERY_BEAT_SCHEDULE = {
         'schedule': 2419200.0,
     },
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
