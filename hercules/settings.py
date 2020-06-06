e43b2cc7f42e0d472d7da27719ecb748fc6ff0ff
TBury <tomasz.bury@onet.eu>
2020-06-06 12:32:47 +0200

    Revert "Add heroku allowed host"
    This reverts commit 0c36e84e44d19f5f46579e4185a4404c5067381e.

--- hercules/settings.py
+++ hercules/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'm^%^y3_8e&$l)q&mfuihrm_gihvl#g7@k&gpfg4bwx8v9kpu#8'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://hercules-project.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
