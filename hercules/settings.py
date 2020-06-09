a4dac4210ffc27d4a888e2ab6f14de4a88dd2782
TBury <tomasz.bury@onet.eu>
2020-06-09 12:06:23 +0200

    update heroku db

--- hercules/settings.py
+++ hercules/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 from django.conf.urls.static import static
 from django.conf import settings
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -29,7 +30,8 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['hercules-project.herokuapp.com', '127.0.0.1']
 
-
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 # Application definition
 
 INSTALLED_APPS = [
