9c9f7c108788128a5c0e5f027aaea9284f0ed4f4
TBury <tomasz.bury@onet.eu>
2020-06-10 17:07:13 +0200

    repair waybill

--- hercules_app/tasks.py
+++ hercules_app/tasks.py
@@ -117,7 +117,7 @@ def get_waybill_info(first_screen_path, end_screen_path, waybill_id, bind=True):
 
 def create_temp_image(image):
     temp_image = BytesIO()
-    temp_image.write(image)
+    image.save(temp_image, format="png")
     temp_image.flush()
     return temp_image
 
