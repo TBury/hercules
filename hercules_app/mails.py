c41ee058acc60f00e090b06f92255b33c7e5588c
TBury <tomasz.bury@onet.eu>
2021-06-07 20:57:54 +0200

    Try to fix mailgun url

--- hercules_app/mails.py
+++ hercules_app/mails.py
@@ -1,13 +1,15 @@
+from django.conf import settings
+
 from requests import post
 import requests
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
 
 def send_contact_mail(sender, message):
-    url = os.getenv("MAILGUN_API_URL")
-    key = os.getenv("MAILGUN_API_KEY")
+    url = settings.MAILGUN_API_URL
+    key = settings.MAILGUN_API_KEY
+    print(url)
+    print(key)
     return requests.post(
         url,
         auth = ("api", key),
